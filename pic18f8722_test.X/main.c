ed53eb3340e0e1584f4d8ddf7fe41906592220d5
John Schisler <john_schisler@hotmail.com>
2014-03-06 15:19:19 -0500

    Added test code.

--- pic18f8722_test.X/main.c
+++ pic18f8722_test.X/main.c
@@ -3,11 +3,11 @@
 /******************************************************************************/
 
 #if defined(__XC)
-    #include <xc.h>        /* XC8 General Include File */
+#include <xc.h>        /* XC8 General Include File */
 #elif defined(HI_TECH_C)
-    #include <htc.h>       /* HiTech General Include File */
+#include <htc.h>       /* HiTech General Include File */
 #elif defined(__18CXX)
-    #include <p18cxxx.h>   /* C18 General Include File */
+#include <p18cxxx.h>   /* C18 General Include File */
 #endif
 
 #if defined(__XC) || defined(HI_TECH_C)
@@ -33,14 +33,29 @@
 
 /******************************************************************************/
 /* Main Program                                                               */
+
 /******************************************************************************/
 
-void main(void)
-{
+void main(void) {
     char packet[] = {'T', 'e', 's', 't'};
 
     unsigned char i = 0;
     unsigned char packet_size = sizeof (packet) / sizeof (packet[0]);
+    unsigned char cArray[10]
+            = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    // temporary local array
+    unsigned char writtenArray[10];
+
+    // local variables for all test cases and their initialisation
+    unsigned char chData;
+    unsigned char *pReadArray;
+    unsigned char *pWriteArray;
+    unsigned char j = 0;
+    unsigned char i = 0;
+    unsigned char l = 0;
+    unsigned char k = 0;
+    pReadArray = pWriteArray = writtenArray;
 
     /* Configure the oscillator for the device */
     ConfigureOscillator();
@@ -49,7 +64,7 @@ void main(void)
     InitApp();
 
     /* TODO <INSERT USER APPLICATION CODE HERE> */
-    
+    /*
     while(1)
     {
         i = 0;
@@ -62,6 +77,30 @@ void main(void)
 
         Delay10KTCYx(1000);
     }
+     */
+    TRISD = 0;
+    PORTD = 0;
+    k = 0;
 
+    while (k < 100) {
+        for (j = 0; j < 100; j++) {
+            i = 0;
+            do {
+                if (vUARTIntStatus.UARTIntTxBufferEmpty)
+                    UARTIntPutChar(cArray[i++]);
+            } while (i < 10);
+        }
+
+        if (!(vUARTIntStatus.UARTIntRxError) &&
+            !(vUARTIntStatus.UARTIntRxOverFlow) &&
+            !(vUARTIntStatus.UARTIntRxBufferEmpty)) {
+                if (UARTIntGetChar(&chData))
+                    PORTD = chData;
+        }
+
+        k++;
+    }
+
+    while (1);
 }
 
